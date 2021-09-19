32f22d32a60f5027c6a389b417d966b2a7e1673c
cipriana <ciprianaluis264@gmail.com>
2021-09-19 03:15:21 -0300

    novas modificacoes

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let botaoEnviar = document.getElementById(botaoEnviar);
-let formulario = document.getElementsByClassName("contato_formulario");
+let botaoEnviar = document.getElementById("botaoEnviar");
+let formularioValido = document.getElementsByClassName("contato_formulario")
 
-botaoEnviar.addEventListener("click", (event) => {
+botaoEnviar.addEventListener('click', (event) => {
+  event.preventDefault();
+  //let  formularioValido = true;
+
   let campoNome = document.getElementById("nome");
   let mensagemNome = document.getElementById("mensagemNome");
 
@@ -15,45 +18,64 @@ botaoEnviar.addEventListener("click", (event) => {
 
   if (campoNome.value.length === 0) {
     mensagemNome.innerHTML = "Digite seu nome";
+    //formularioValido = false;
+
   } else {
-    mensagem.innerHTML = "";
+    mensagemNome.innerHTML = "";
   }
 
-  //campoEmail.value = "";
+   campoEmail.value = "";
+
   if (campoEmail.value === "") {
     mensagemEmail.innerHTML = "Digite seu email";
+   // formularioValido = false;
   } else {
     mensagemEmail.innerHTML = "";
   }
-
+  
+  campoMensagem.value= "";
+   
   if (campoMensagem.value.length === 0) {
     mensagemMensagem.innerHTML = "Digite sua mensagem";
+    //formularioValido = false;
   } else {
     mensagemMensagem.innerHTML = "";
+  //if (
+     //campoMensagem.value.length === 0 &&
+     //campoEmail.value === "" &&
+     //campoNome.value.length === 0 
+     //) {
 
-    event.preventDefault();
-  }
-});
+     }
+  //if (!formularioValido) {
+    //event.preventDefault();
+    }
+  );
 
 let promessa = fetch("https://api-github.com/users/ciprianalombendo/repos");
 
 let containerRepositorios = document.getElementById("containerGithub");
 
 promessa
-  .then(function (retorno) {
+  .then(function (retorno) { 
     return retorno.json();
   })
 
   .then(function (repositorios) {
-    let nomes = {};
+    let nomes = [];
 
     for (let repositorio of repositorios) {
-      console.log(repositorio.html_url);
+      let url = repositorio.html_url;
+      let nome = repositorio.nome;
 
-      nomes.push(
-        "<li>" + repositorio.name + " " + repositorio.html_url + "</li>"
-      );
+      nomes.push(`
+        <li>
+          ${nome} <a href="${url}">${url}</a>
+        </li>
+      `);
+        //"<li>" + repositorio.name + " " + repositorio.html_url + "</li>"
+      
     }
 
     containerRepositorios.innerHTML = nomes;
-  });
+      });
