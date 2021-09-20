370b4a1c6b78cffe16c02679bb04b5850b77ac0a
cipriana <ciprianaluis264@gmail.com>
2021-09-20 12:11:28 -0300

    chamando api

--- script.js
+++ script.js
@@ -1,58 +1,26 @@
-let botaoEnviar = document.getElementById("botaoEnviar");
-let formularioValido = document.getElementsByClassName("contato_formulario")
+function validar() {
+  let nome = formulario.nome.value;
+  let email= formulario.email.value;
+  let mensagem = formulario.mensagem.value;
 
-botaoEnviar.addEventListener('click', (event) => {
-  event.preventDefault();
-  //let  formularioValido = true;
+if(nome === "") {
+  alert('Preencha o campo nome.');
+  formulario.nome.focus();
+  return false;
+}
+if(email === "") {
+  alert('Preencha o campo email.');
+  formulario.email.focus();
+  return false;
+}
+if(mensagem === "") {
+  alert('Preencha o campo mensagem.');
+  formulario.mensagem.focus();
+  return false;
+}
+}
 
-  let campoNome = document.getElementById("nome");
-  let mensagemNome = document.getElementById("mensagemNome");
-
-  let campoEmail = document.getElementById("email");
-  let mensagemEmail = document.getElementById("mensagemEmail");
-
-  let campoMensagem = document.getElementById("mensagem");
-  let mensagemMensagem = document.getElementById("mensagemMensagem");
-
-  campoNome.value = "";
-
-  if (campoNome.value.length === 0) {
-    mensagemNome.innerHTML = "Digite seu nome";
-    //formularioValido = false;
-
-  } else {
-    mensagemNome.innerHTML = "";
-  }
-
-   campoEmail.value = "";
-
-  if (campoEmail.value === "") {
-    mensagemEmail.innerHTML = "Digite seu email";
-   // formularioValido = false;
-  } else {
-    mensagemEmail.innerHTML = "";
-  }
-  
-  campoMensagem.value= "";
-   
-  if (campoMensagem.value.length === 0) {
-    mensagemMensagem.innerHTML = "Digite sua mensagem";
-    //formularioValido = false;
-  } else {
-    mensagemMensagem.innerHTML = "";
-  //if (
-     //campoMensagem.value.length === 0 &&
-     //campoEmail.value === "" &&
-     //campoNome.value.length === 0 
-     //) {
-
-     }
-  //if (!formularioValido) {
-    //event.preventDefault();
-    }
-  );
-
-let promessa = fetch("https://api-github.com/users/ciprianalombendo/repos");
+let promessa = fetch("https://api.github.com/users/ciprianalombendo/repos");
 
 let containerRepositorios = document.getElementById("containerGithub");
 
@@ -73,9 +41,8 @@ promessa
           ${nome} <a href="${url}">${url}</a>
         </li>
       `);
-        //"<li>" + repositorio.name + " " + repositorio.html_url + "</li>"
       
     }
 
     containerRepositorios.innerHTML = nomes;
-      });
+  });
